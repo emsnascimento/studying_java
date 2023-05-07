371cabb63eb1b3b3b618508c714108f790de6c87
Emilly Nascimento <Emilly.VitoriaCostaNascimento-ext@ab-inbev.com>
2023-05-07 15:47:31 -0300

    Exercicio - Decomposição em fatores primos com ArrayList

--- TrialDivision.java
+++ TrialDivision.java
@@ -1,31 +1,46 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TrialDivision {
-  public static void FindThePrimeFactors(int n) {
+  public static Integer[] FindThePrimeFactors(int n) {
+    ArrayList<Integer> factorsList = new ArrayList<>();
+
     while (n % 2 == 0) {
-      System.out.print(2 + " ");
+      factorsList.add(2);
       n /= 2;
     }
 
     for (int i = 3; i <= Math.sqrt(n); i += 2) {
       while (n % i == 0) {
-        System.out.print(i + " ");
+        factorsList.add(i);
         n /= i;
       }
     }
 
     if (n > 2) {
-      System.out.print(n);
+      factorsList.add(n);
     }
+
+    return factorsList.toArray(new Integer[0]);
   }
 
   public static void main(String[] args) {
-    try (Scanner scanner = new Scanner(System.in)) {
-      final int T = scanner.nextInt();
+    Scanner scanner = new Scanner(System.in);
+
+    if (scanner.hasNextInt()) {
+      int T = scanner.nextInt();
 
       for (int t = 0; t < T; t++) {
         int n = scanner.nextInt();
-        FindThePrimeFactors(n);
+        Integer[] factors = FindThePrimeFactors(n);
+        for (int i = 0; i < factors.length; i++) {
+          System.out.print(factors[i]);
+          if (i < factors.length - 1) {
+            System.out.print(" ");
+          }
+        }
+        
+        System.out.println();
       }
     }
   }
